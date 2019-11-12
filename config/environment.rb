cf6141906951c5599900acd74a8479cfa5afb935
cep104 <51180851+cep104@users.noreply.github.com>
2019-11-12 15:21:14 -0600

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -14,3 +14,12 @@ SQL
 
 DB[:conn].execute(sql)
 DB[:conn].results_as_hash = true
+
+#STEP ONE: Setting up our database
+#creating the database 
+#drops songs to avoid an error
+#creating the songs table 
+#results_as_hash: when a SELECT statement is executed, don't 
+#return a database row as an array, return it as a hash with the column names as heys.
+#instead of this: [[1, "Hello", "25"]]
+#it returns this {"id"=>1, "name"=>"Hello", "album"=>"25", 0 => 1, 1 => "Hello", 2 => "25"}
